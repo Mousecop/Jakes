ee5435d0be37b7a0ff478674d5ae5cd47fd54506
Mousecop <joaskew13@gmail.com>
2017-02-15 17:44:56 -0500

    Added Public folder and moved files into it

--- Public/testApp.js
+++ Public/testApp.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('assests'));
+app.use('/assets', express.static('assets'));
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', (req, res) => {
